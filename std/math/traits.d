002ebf56cfd1e7b4d5027eea15ed65ae494753ac
dbey2 <85197648+dbey2@users.noreply.github.com>
2023-10-24 12:10:18 +0300

    Update traits.d
    When we look at the assembly code, it will be clear that the new sgn() function is faster.

--- std/math/traits.d
+++ std/math/traits.d
@@ -694,8 +694,7 @@ $(D x > 0), and $(NAN) if x==$(NAN).
 F sgn(F)(F x) @safe pure nothrow @nogc
 if (isFloatingPoint!F || isIntegral!F)
 {
-    // @@@TODO@@@: make this faster
-    return x > 0 ? 1 : x < 0 ? -1 : x;
+    return (x > 0) - (x < 0);
 }
 
 ///
